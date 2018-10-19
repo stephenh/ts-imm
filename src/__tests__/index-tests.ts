20cdf63e5cc3e5e56719c62f5fb20d091c822202
Stephen Haberman <stephen.haberman@gmail.com>
2018-10-18 20:49:16 -0500

    Pretty nice, clean setup.

--- src/__tests__/index-tests.ts
+++ src/__tests__/index-tests.ts
@@ -1,24 +1,35 @@
-import {Properties, RequiredData} from "../index";
+import {Imm, imm} from "../index";
 
-type Foo = Properties<SimpleData>;
+class SimpleData extends Imm<SimpleData> {
+  @imm public name!: string;
+  @imm public address?: string;
 
-class SimpleData {
-  public name!: string;
-  public address?: string;
-
-  constructor(data: Properties<SimpleData>) {
-  }
-
-  public copy(update: Partial<SimpleData>): this {
-    return this;
+  public mutate(): this {
+    return this.copy({name: "new name"});
   }
 }
 
 describe("imm", () => {
-
   it("can create a new instance", () => {
     const d = new SimpleData({name: "a"});
+    expect(d.name).toEqual("a");
+    expect(d.address).toBeUndefined();
   });
 
+  it("can mutate a property with a method", () => {
+    const a = new SimpleData({name: "a"});
+    expect(a.name).toEqual("a");
+    const b = a.mutate();
+    expect(a.name).toEqual("a");
+    expect(b.name).toEqual("new name");
+  });
+
+  it("can mutate a property with the generic copy constructor", () => {
+    const a = new SimpleData({name: "a"});
+    expect(a.address).toBeUndefined();
+    const b = a.copy({address: "b"});
+    expect(a.address).toBeUndefined();
+    expect(b.address).toEqual("b");
+  });
 
 });
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,67 @@
+import {Map} from "immutable";
 
 // https://medium.com/dailyjs/typescript-create-a-condition-based-subset-types-9d902cea5b8c
 type NotSubTypeKeys<T, Condition> = {
   [K in keyof T]: T[K] extends Condition ? never : K
 }[keyof T];
 
-// https://stackoverflow.com/questions/50829805/get-keyof-non-optional-property-names-in-typescript
-type NonOptionalKeys<T> = {
-  [K in keyof T]-?: undefined extends T[K] ? never : K
-}[keyof T];
-
 type AnyFunction = (_: any) => any;
 type PropertyKeys<T> = NotSubTypeKeys<T, AnyFunction>;
 
+/** The non-method properties of T. */
 export type Properties<T> = Pick<T, PropertyKeys<T>>;
-export type RequiredData<T> = Pick<T, NonOptionalKeys<T> & PropertyKeys<T>>;
 
-export class Index {
+/** Data we collect as the decorators run. */
+class ImmClassData {
+  /** @return the `ImmData` for `target`, which should be the prototype. */
+  public static of(proto: any): ImmClassData {
+    if (proto.__imm === undefined) {
+      proto.__imm = new ImmClassData();
+    }
+    return proto.__imm as ImmClassData;
+  }
 
+  /** A list to track properties as @imm sees them. */
+  public properties: string[] = [];
+}
 
+interface Constructor {
+  new(...args: any[]): any;
+}
+
+/** A base class for data classes so they get the constructor and copy method for free. */
+export class Imm<T> {
+
+  private readonly map: Map<any, any>;
+
+  constructor(data: Properties<T>) {
+    // Even though our professed parameter is is Properties<T>, the copy constructor
+    // lies and passes in a map so that we can use it directly.
+    this.map = data instanceof Map ? data as any as Map<any, any> : this.newMap(Map(), data);
+  }
+
+  public copy(data: Partial<T>): this {
+    return new (this.constructor as Constructor)(this.newMap(this.map, data));
+  }
+
+  private newMap(map: Map<any, any>, data: any): Map<any, any> {
+    ((this as any).__imm as ImmClassData).properties.forEach(p => {
+      if (data.hasOwnProperty(p)) {
+        map = map.set(p, data[p]);
+      }
+    })
+    return map;
+  }
+}
+
+/** Annotates a property as immutable/for inclusion in the copy constructor. */
+export function imm(target: any, propertyKey: string): any {
+  ImmClassData.of(target).properties.push(propertyKey);
+  Object.defineProperty(target, propertyKey, {
+    configurable: false,
+    enumerable: true,
+    get(): any {
+      return this.map.get(propertyKey);
+    },
+  });
 }
